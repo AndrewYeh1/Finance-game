8f98dbbba46e4a222ae695fb249394f401dce799
AndrewYeh1 <72671554+AndrewYeh1@users.noreply.github.com>
2021-04-16 00:39:44 -0600

    Added new functions

--- building_interior/bank/BankInt.gd
+++ building_interior/bank/BankInt.gd
@@ -14,11 +14,11 @@ func _ready():
 func _input(ev):
 	if Input.is_key_pressed(KEY_ESCAPE) and menuOpen == false:
 		get_tree().change_scene("res://Play/Play.tscn")
+	if Input.is_key_pressed(KEY_ESCAPE) and menuOpen == true:
+		$"CanvasLayer/bankMenu".visible = false
+		menuOpen = not menuOpen
 	if Input.is_key_pressed(KEY_SPACE):
 		if menuOpen == false:
 			if $player.location == "BankTeller":
 				$"CanvasLayer/bankMenu".visible = true
 				menuOpen = not menuOpen
-		else:
-			$"CanvasLayer/bankMenu".visible = false
-			menuOpen = not menuOpen
--- building_interior/store/storeInterior.gd
+++ building_interior/store/storeInterior.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var menuOpen = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,9 +13,12 @@ func _ready():
 
 
 func _input(event):
-	if Input.is_key_pressed(KEY_ESCAPE):
+	if Input.is_key_pressed(KEY_ESCAPE) and menuOpen == false:
+		print("AIGWDW")
 		get_tree().change_scene("res://Play/Play.tscn")
+	elif Input.is_key_pressed(KEY_ESCAPE) and menuOpen == true:
+		$CanvasLayer/menu.hide()
+		menuOpen = false
 	if Input.is_key_pressed(KEY_SPACE) and $player.location == "StoreInt":
 		$CanvasLayer/menu.visible = not $CanvasLayer/menu.visible
-	elif Input.is_key_pressed(KEY_SPACE):
-		$CanvasLayer/menu.hide()
+		menuOpen = true
